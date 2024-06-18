494f68cdc7b3825ad724312f1d88c87defe85b1a
Kirilenko <n@kirilenko.com>
2024-06-18 16:01:09 +0400

    feat: add package vanilla-sample

--- apps/web-client/src/app/app.tsx
+++ apps/web-client/src/app/app.tsx
@@ -3,25 +3,30 @@ import { Global } from '@emotion/react'
 
 import { env } from '@app/config'
 import viteLogo from '@public/vite.svg'
+import { log } from '@repo/vanilla-sample'
 
 import './styles/tailwind.css'
 import { AppStyle } from './styles/app.style'
 import { GlobalStyles } from './styles/global.style'
 
-const App: FC = () => (
-  <>
-    <Global styles={GlobalStyles} />
-    <AppStyle>
-      <img src={viteLogo} className="logo" alt="Vite logo" />
-      <span>
-        template.<span className="text-fun">react</span>-app.turbo
-      </span>
-    </AppStyle>
-    <span className="hidden">{env.VITE_TIMESTAMP}</span>
-    <span className="hidden">{env.VITE_LOCAL_KEY}</span>
-    <span className="hidden">{env.VITE_GLOBAL_KEY}</span>
-    <span className="hidden">{env.VITE_GLOBAL_KEY_FROM_ADDITIONAL_ENV}</span>
-  </>
-)
+const App: FC = () => {
+  log('Hello from vanilla-sample')
+
+  return (
+    <>
+      <Global styles={GlobalStyles} />
+      <AppStyle>
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+        <span>
+          template.<span className="text-fun">react</span>-app.turbo
+        </span>
+      </AppStyle>
+      <span className="hidden">{env.VITE_TIMESTAMP}</span>
+      <span className="hidden">{env.VITE_LOCAL_KEY}</span>
+      <span className="hidden">{env.VITE_GLOBAL_KEY}</span>
+      <span className="hidden">{env.VITE_GLOBAL_KEY_FROM_ADDITIONAL_ENV}</span>
+    </>
+  )
+}
 
 export default App
---
+++ packages/vanilla-sample/.eslintrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: [
+    '@repo/eslint-config/vanilla.cjs',
+  ],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+}
